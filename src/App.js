f25798ddb0c751e1f8854796723f462d7d460698
developertamimtaz <farhanahmedtaz21@gmail.com>
2024-02-05 21:50:09 +0600

    react-start

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done22 <code>src/App.js</code> and save to reload.
-        </p>
-        
+       <p>I am a React Person</p>
       </header>
     </div>
   );
