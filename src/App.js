9c9405b7619195914209b2294e23c7defd1198b4
developertamimtaz <farhanahmedtaz21@gmail.com>
2024-02-06 11:31:18 +0600

    react-done

--- src/App.js
+++ src/App.js
@@ -7,9 +7,28 @@ function App() {
     <div className="App">
       <header className="App-header">
        <p>I am a React Person</p>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  const personStyle={
+    border: '2px solid red',
+    margin: '10px',
+    padding: '10px',
+    borderRadius: '10px'
+  }
+  return(
+    <div style={personStyle}>
+      <h1>Name: Sakib Al Hasan</h1>
+      <h3>Hero of the year</h3>
+    </div>
+  )
 }
 
 export default App;
